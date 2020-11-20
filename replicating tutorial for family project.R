3a0fe6e0f989da2efb204e90e4c5339731f88eac
zpashkutz <74225563+zpashkutz@users.noreply.github.com>
2020-11-19 22:51:00 -0800

    Add files via upload

---
+++ replicating tutorial for family project.R
@@ -0,0 +1,173 @@
+# first need to do rarefaction
+# Rarefying - normalizes read depth across all samples. 
+# Allows for an equal comparison across different sample types, at the risk of excluding rarer taxa
+# A "good" rarefaction depth should minimize sample loss while maximizing OTU richness.
+
+# get quartile ranges for rarefaction
+transOTU <- rowSums(t(OTU_table[,2:96])) 
+Q10 <- quantile(transOTU[order(transOTU, decreasing = TRUE)], 0.10)
+Q15 <- quantile(transOTU[order(transOTU, decreasing = TRUE)], 0.15)
+
+# we can use these numbers to set a range (e.g., Q10) to keep samples at that depth (e.g., 90th percentile)
+barplot(sort(transOTU), ylim = c(0, max(transOTU)), 
+        xlim = c(0, NROW(transOTU)), col = "Blue", ylab = "Read Depth", xlab = "Sample") 
+abline(h = c(Q10, Q15), col = c("red", "pink"))
+plot.new()
+
+# did we sequence each sample adequately? - ideally, you see the lines plateau meaning you sampled the community!
+# if not, you may need to sequencer to greater depth for each sample (if lines are linear)
+rarecurve(t(OTU_table[,2:96]), step = 100, cex = 0.5)
+abline(v = c(Q10, Q15), col = c("red", "pink"))
+
+# you will need to make a BIG decision here on where to draw the rarefaction cutoff
+# samples to the left of the red or pink lines will be thrown out
+rared_OTU <- as.data.frame((rrarefy.perm(t(OTU_table[,2:96]), sample = Q10, n = 100, round.out = T)))
+
+# This only keeps the samples that meet the rarefaction cutoff.
+rared_OTU <- as.data.frame(rared_OTU[rowSums(rared_OTU) >= Q10 - (Q10 * 0.1), colSums(rared_OTU) >= 1])
+
+
+############### ############### ############### 
+############### Alpha diversity ###############
+############### ############### ############### 
+
+library(vegan)
+library(tidyverse)
+# outside of this, we can look at the richness of each sample, or how many taxa are in each sample
+richness <- as.data.frame(specnumber(rared_OTU))
+colnames(richness) <- c("speciesrich")
+
+# Merge with metadata to create a plot.
+nerged_rich <- merge(richness, Microbiome_mapping, by = 0)
+rownames(nerged_rich) <- nerged_rich$Row.names
+nerged_rich$Row.names <- NULL
+
+# now for alpha diversity
+# we will use Shannon diversity as a method of alpha-diversity.
+shannon <- as.data.frame(diversity(rared_OTU, index = "shannon"))
+colnames(shannon) <- c("alpha_shannon")
+
+# Merge with metadata to create a plot.
+nerged_alpha <- merge(nerged_rich, shannon, by = 0)
+
+#filter data frame to only have individuals being compared.
+#In this case, fam1 members
+nerged_alpha <- filter(nerged_alpha, Family == 'Fam1')
+
+#Plotting alpha diversity:
+#Change 'factor_X' to categorical metadata factor you wish to plot. Press tab after the '$' sign.
+factor_X <- as.factor(nerged_alpha$Individual)
+
+#plot richness
+p1 <- ggplot(data = nerged_alpha) +
+  aes(x = factor_X, y = nerged_alpha$speciesrich, 
+      fill = factor_X) +
+  geom_boxplot(outlier.shape = NA, lwd = 1) +
+  labs(title = 'Species Richness',
+       #Change x-axis label and legend title to metadata factor of interest.
+       x = 'Family 1 Individuals', y = 'Species Richness', fill = 'Individual') +
+  theme_classic(base_size = 14, base_line_size = 1) +
+  geom_jitter(width = .2) +
+  theme(legend.position = "none")
+
+# preview the figure
+p1
+
+# plot alpha diversity
+p2 <- ggplot(data = nerged_alpha) +
+  aes(x = factor_X, y = nerged_alpha$alpha_shannon, 
+      fill = factor_X) +
+  geom_boxplot(outlier.shape = NA, lwd = 1) +
+  labs(title = 'Alpha Diversity',
+       #Change x-axis label and legend title to metadata factor of interest.
+       x = 'Family 1 Individuals', y = 'Shannon Diversity Index', fill = 'Individual') +
+  theme_classic(base_size = 14, base_line_size = 1) +
+  geom_jitter(width = .2) +
+  theme(legend.position = "none")
+
+#preview the figure
+p2
+
+
+
+############### ############### ############### 
+###############  Beta diversity ###############
+############### ############### ############### 
+help(metaMDS)
+NMDS1 <- metaMDS(rared_OTU, distance = "bray", k = 2, trymax = 500)
+
+# Extract the two axes of the NMDS to plot the x and y coordinates
+coordinates <- data.frame(NMDS1$points[,1:2])
+
+# Quick glance at the NMDS plot
+# not too exciting right? well we need to add Microbiome_mapping!!
+plot(x = coordinates$MDS1, y = coordinates$MDS2)
+
+# so merge NMDS axes coordinates with Microbiome_mapping
+nmds_plus_Microbiome_mapping <- merge(coordinates, Microbiome_mapping, by = 0)
+
+# choose the factor you are interested in (this time let's check the "Family" effects)
+Factor_x <- as.factor(nmds_plus_Microbiome_mapping$Family)
+
+# Plot
+ggplot(data = nmds_plus_Microbiome_mapping) +
+  aes(x = MDS1, y = MDS2, color = Family)+ #Creates and colors legend to match, modify after $ here.
+  geom_point(size = 3) +
+  labs(col = "Family") + #Renames legend, modifiable. 
+  theme_bw()
+
+# for statistical tests, we need to get the data tidied up a bit
+# so let's subset the Microbiome_mapping and keeps only the samples that passed filtering and rarefaction
+filtersamples <- rownames(rared_OTU)
+filtermapping <- subset(Microbiome_mapping, rownames(Microbiome_mapping) %in% filtersamples)
+
+# Now test for differences among beta-diversity
+# one thing we can do is a permanova (permutational ANOVA) 
+# permutated analysis of variance, or a type of statistical test, useful for large multivariate analyses.
+#Note: this may not be the proper statistical test for your hypothesis, but is a good place to start for microbiome data.
+
+# let's check out the function in R that does this - you can brush up on the stats in the bottom right
+?adonis
+
+# try some different factors
+adonis(data = filtermapping, formula = rared_OTU ~ Family 
+       / Individual + Day + Day:Family,
+       permutations = 999, method = "bray")
+
+adonis(data = filtermapping, formula = rared_OTU ~ Family
+       /Individual/Day,
+       permutations = 999, method = "bray")
+
+# again, the adonis test is dependent on what you think is happening in the microbiomes so adjsut accordingly
+
+# if we want we can go back to our plot and add these results in
+# To replot: 
+# Change the R2 and p-value numbers to match!
+ggplot(data = nmds_plus_Microbiome_mapping) +
+  aes(x = MDS1, y = MDS2, color = Factor_x, fill = Factor_x) +
+  geom_point() +
+  labs(fill = "Family") +
+  guides(color = FALSE) +
+  ggtitle("NMDS of Families on Oral Microbiome") +
+  theme_classic(base_size = 14, base_line_size = .5) +
+  geom_polygon(alpha = 0.5)
+
+###############################################################################
+
+install.packages('vegan')
+library(vegan)
+
+setwd("/Users/Zach/Desktop/Family Project")
+
+Microbiome_mapping <- read.delim('Microbiome_mapping.txt')
+head(Microbiome_mapping)
+
+OTU_table <- read.delim('OTU_table.txt')
+head(OTU_table)
+
+#Manova
+OTU_Table.matrix<-as.matrix(OTU_Table[,])
+taxonomy$B<-as.factor(taxonomy$B)
+
+OTU_Table.manova <- manova(OTU_Table.matrix~as.factor(B), data=taxonomy)
+summary(OTU_Table.manova)
